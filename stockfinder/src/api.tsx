78c0fbca086777464888b3e7832039060d2e65be
DeepBlueIBR <romeosa@hotmail.gr>
2026-03-03 23:28:48 +0200

    fixed all endpoint to hit in the backend protecting the key

--- stockfinder/src/api.tsx
+++ stockfinder/src/api.tsx
@@ -15,7 +15,6 @@ export const searchCompanies = async (query: string) => {
                 },
             }
         );
-        console.log(data);
         return data;
     } catch (error: unknown) {
         if ((error as any).isAxiosError) {
@@ -28,34 +27,17 @@ export const searchCompanies = async (query: string) => {
     }
 }
 
-// export const searchCompanies = async (query: string) => {
-//     try {
-//         const { data } = await axios.get<SearchResponse>(
-//             `https://financialmodelingprep.com/stable/search-name?query=${query}&apikey=${process.env.REACT_APP_API_KEY}`
-
-//             //`https://financialmodelingprep.com/api/v3/search-ticker?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
-//         );
-//         console.log(data);
-//         return data;
-//     } catch (error: unknown) {
-//         // Use manual type guard if `axios.isAxiosError` doesn't exist
-//         if ((error as any).isAxiosError) {
-//             console.log("error message: ", (error as any).message);
-//             return (error as any).message;
-//         } else {
-//             console.log("unexpected error: ", error);
-//             return "An unexpected error has occurred.";
-//         }
-//     }
-// };
 
 export const getCompanyProfile = async (query: string): Promise<CompanyProfile[] | undefined> => {
   try {
     const response = await axios.get<CompanyProfile[]>(
-      `https://financialmodelingprep.com/stable/profile?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+      `${process.env.REACT_APP_API_URL}/stock/company-profile/${query}`,
+      {
+        headers:{
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
     );
-
-    console.log(response.data); 
     return response.data; 
   } catch (error: any) {
     console.error("Error message from API:", error.message);
@@ -63,32 +45,39 @@ export const getCompanyProfile = async (query: string): Promise<CompanyProfile[]
   }
 };
 
-export const getKeyMetrics = async (query: string ) => {
+export const getKeyMetrics = async (query: string) => {
     try {
         const data = await axios.get<CompanyKeyMetrics[]>(
-        `https://financialmodelingprep.com/stable/key-metrics-ttm?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
-    )
-        console.log(data);
+            `${process.env.REACT_APP_API_URL}/stock/key-metrics/${query}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            }
+        );
         return data;
     } catch (error: any) {
-        console.log("error meesage from API", error.message);
+        console.log("error message from API", error.message);
     }
-}
+};
+
 
 export const getIncomeStatement = async (query: string ) => {
     try {
         const data = await axios.get<CompanyIncomeStatement[]>(
-        `https://financialmodelingprep.com/stable/income-statement?symbol=${query}&limit=1&apikey=${process.env.REACT_APP_API_KEY}`
-
-            //`https://financialmodelingprep.com/stable/income-statement?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
-
-    )
-        console.log(data);
+        `${process.env.REACT_APP_API_URL}/stock/income-statements/${query}`,
+        { 
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        }
+        );
         return data;
     } catch (error: any) {
         console.log("error meesage from API", error.message);
     }
 }
+
 
 export type ApiError = {
   error: string;
@@ -98,12 +87,17 @@ export const isApiError = (value: any): value is ApiError => {
   return value && typeof value === 'object' && 'error' in value;
 };
 
+
 export const getBalanceSheet = async (query: string ) => {
     try {
         const data = await axios.get<CompanyBalanceSheet[]>(
-        `https://financialmodelingprep.com/stable/balance-sheet-statement?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
-    )
-        console.log(data);
+        `${process.env.REACT_APP_API_URL}/stock/balance-sheet/${query}`,
+        {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        }
+        );
         return data;
     } catch (error: any) {
         console.log("error meesage from API", error.message);
@@ -113,9 +107,13 @@ export const getBalanceSheet = async (query: string ) => {
 export const getCashFlowStatement = async (query: string ) => {
     try {
         const data = await axios.get<CompanyCashFlow[]>(
-        `https://financialmodelingprep.com/stable/cash-flow-statement?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
-    )
-        console.log(data);
+        `${process.env.REACT_APP_API_URL}/stock/cash-flow-statements/${query}`,
+        {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        }
+    );
         return data;
     } catch (error: any) {
         console.log("error meesage from API", error.message);
@@ -137,7 +135,6 @@ export const getTenK = async (query: string ) => {
        ` https://financialmodelingprep.com/stable/sec-filings-search/symbol?symbol=${query}&from=${fromDate}&to=${toDate}&apikey=${process.env.REACT_APP_API_KEY}`
         //    `https://financialmodelingprep.com/stable/sec-filings-search/symbol?symbol=${query}&from=${fromDate}&to=${toDate}&apikey=${process.env.REACT_APP_API_KEY}`
     )
-        console.log(data);
         return data;
     } catch (error: any) {
         console.log("error meesage from API", error.message);
